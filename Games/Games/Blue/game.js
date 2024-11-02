661009d28e9056f57dba005ca9d9bdd4a1018495
Azynokel <105823522+Azynokel@users.noreply.github.com>
2024-11-02 21:01:58 +0100

    Delete Games directory

--- Games/Games/Blue/game.js
+++
@@ -1,9 +0,0 @@
-export class Game {
-    constructor(canvas) {
-        this.canvas = canvas;
-    }
-    draw(ctx) {
-        ctx.fillStyle = 'blue';
-        ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-    }
-}
--- Games/Games/Green/game.js
+++
@@ -1,9 +0,0 @@
-export class Game {
-    constructor(canvas) {
-        this.canvas = canvas;
-    }
-    draw(ctx) {
-        ctx.fillStyle = 'green';
-        ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-    }
-}
